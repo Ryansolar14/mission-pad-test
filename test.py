286f364921864d31ca08a7731021ad09f3693610
Ryansolar14 <76922192+Ryansolar14@users.noreply.github.com>
2022-10-20 09:12:57 -0400

    Create Test.py
    Create a file that tests mission pad detection.

---
+++ test.py
@@ -0,0 +1,4 @@
+takeoff()
+
+pad_num = mission_pad() //Assign detected mission pad number to the pad_num integer variable
+print("Mission pad detected: #" + str(pad_num)); //Print the mission pad number to the console
