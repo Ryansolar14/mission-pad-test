4c12a9813def1ea469739ddfb99c76989d1c52fe
Ryansolar14 <76922192+Ryansolar14@users.noreply.github.com>
2022-10-20 09:14:37 -0400

    Update test.py
    Correct code.

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-takeoff()
+from tello import * //Import tello library 
 
-pad_num = mission_pad() //Assign detected mission pad number to the pad_num integer variable
+takeoff() //Takeoff
+
+pad_num = get_mission_pad() //Assign detected mission pad number to the pad_num integer variable
 print("Mission pad detected: #" + str(pad_num)); //Print the mission pad number to the console
+
+land() //Land the drone
